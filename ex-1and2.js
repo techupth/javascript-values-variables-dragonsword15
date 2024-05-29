c9eea3c57d7bf92e6b025bc758a1228307e24b7c
pongpetr <suerdragon8995@gmail.com>
2024-05-29 22:24:27 +0700

    BP3-pang

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,14 @@
 //Exercise #1: Variable Creation
 //Start coding here
-
+let name = "Karter"
+let surname = "Anderson"
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kraui"
+const restaurant = "Chef's Table by Chef James"
+const foodBill = 15000;
+const branch = 1500;
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd#4Meeker, Colorado(Co), 81641"
+foodBill = 5000;
+branch = 2500;
+ 
